801b0fd8ea8378721fc0936fa970edea6f67d22a
rumforweb <ebertzz90@gmail.com>
2025-01-14 15:08:17 +0300

    fix script

--- js/steps.js
+++ js/steps.js
@@ -1,6 +1,4 @@
 var currentTab = 0
-
-tab(0)
 showTab(currentTab)
 
 function showTab(n) {
@@ -9,17 +7,14 @@ function showTab(n) {
 
 	if (n == x.length - 1) {
 		document.getElementById('nextBtn').innerHTML = 'Продолжить'
-	} else {
-		document.getElementById('nextBtn').innerHTML = 'Продолжить'
 	}
 
 	currentTab = currentTab + n
-	// if you have reached the end of the form... :
 	if (currentTab >= x.length) {
-		//...the form gets submitted:
 		document.getElementById('regForm').submit()
 		return false
 	}
+
 	showTab(currentTab)
 	fixStepIndicator(n)
 }
