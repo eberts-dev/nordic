8cc2b9e628d8c6cf512deec1ea44640615960d6a
rumforweb <ebertzz90@gmail.com>
2025-01-14 11:58:46 +0300

    +

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,37 @@
 function Menu() {
-  var button = document.getElementById("mobile-btn");
-  var menu = document.getElementById("menu");
-  
-  if (menu.style.display === "block") {
-    button.classList.remove("active");
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-    button.classList.add("active");
-  }
+	var button = document.getElementById('mobile-btn')
+	var menu = document.getElementById('menu')
+
+	if (menu.style.display === 'block') {
+		button.classList.remove('active')
+		menu.style.display = 'none'
+	} else {
+		menu.style.display = 'block'
+		button.classList.add('active')
+	}
 }
 
 function SubMenu() {
-  var sMenu = document.getElementById("menu__sublist");
+	var sMenu = document.getElementById('menu__sublist')
 
-  if (sMenu.style.display === "block") {
-    sMenu.style.display = "none";
-  } else {
-    sMenu.style.display = "block";
-  }
-
+	if (sMenu.style.display === 'block') {
+		sMenu.style.display = 'none'
+	} else {
+		sMenu.style.display = 'block'
+	}
 }
 
 function SubMenuWeb() {
-  var wMenu = document.getElementById("subWeb");
-  var eMenu = document.getElementById("events");
+	var wMenu = document.getElementById('subWeb')
+	var eMenu = document.getElementById('events')
 
-  if (wMenu.style.display === "block") {wMenu.style.display = "none";} else {wMenu.style.display = "block";}
-  
-  if (eMenu.style.display === "block" && wMenu.style.display === "none") {
-      eMenu.style.display === "block"
-  } else if () {
+	if (wMenu.style.display === 'block') {
+		wMenu.style.display = 'none'
+	} else {
+		wMenu.style.display = 'block'
+	}
 
-  }
-
+	if (eMenu.style.display === 'block' && wMenu.style.display === 'none') {
+		eMenu.style.display === 'block'
+	}
 }
-
--- js/slider.js
+++ js/slider.js
@@ -1,70 +1,70 @@
 const swiper = new Swiper('.sw1', {
-  direction: 'horizontal',
-  loop: true,
-  fadeEffect: { 
-    crossFade: true 
-  },
-  speed: 900,
-  effect: "fade",
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+	direction: 'horizontal',
+	loop: true,
+	fadeEffect: {
+		crossFade: true,
+	},
+	speed: 900,
+	effect: 'fade',
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
 
 const swiper2 = new Swiper('.sw2', {
-  direction: 'horizontal',
-  slidesPerView: "auto",
-  centeredSlides: false,
-  spaceBetween: 50,
-  loop: true,
-  breakpoints: {
-    335: {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        spaceBetween: 20,
-        slidesPerView: 1,
-      },
-    },
-    768: {
-      slidesPerView: "auto",
-      centeredSlides: false,
-      slidesPerView: "auto",
-      spaceBetween: 0,
-    },
-    1040: {
-      slidesPerView: "auto",
-      centeredSlides: false,
-      spaceBetween: 60,
-    }
-  }
-});
+	direction: 'horizontal',
+	slidesPerView: 'auto',
+	centeredSlides: false,
+	spaceBetween: 50,
+	loop: true,
+	breakpoints: {
+		335: {
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				spaceBetween: 20,
+				slidesPerView: 1,
+			},
+		},
+		768: {
+			slidesPerView: 'auto',
+			centeredSlides: false,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+		},
+		1040: {
+			slidesPerView: 'auto',
+			centeredSlides: false,
+			spaceBetween: 60,
+		},
+	},
+})
 
 const swiper3 = new Swiper('.sw3', {
-  direction: 'horizontal',
-  centeredSlides: false,
-  slidesPerView: 1,
-  loop: true,
-  spaceBetween: 500,
-  navigation: {
-    nextEl: '.sw3-button-next',
-    prevEl: '.sw3-button-prev',
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 50
-    },
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    },
-    1040: {
-      centeredSlides: false,
-      slidesPerView: 1,
-      loop: true,
-      spaceBetween: 500,
-    }
-  }
-});
+	direction: 'horizontal',
+	centeredSlides: false,
+	slidesPerView: 1,
+	loop: true,
+	spaceBetween: 500,
+	navigation: {
+		nextEl: '.sw3-button-next',
+		prevEl: '.sw3-button-prev',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 50,
+		},
+		480: {
+			slidesPerView: 1,
+			spaceBetween: 30,
+		},
+		1040: {
+			centeredSlides: false,
+			slidesPerView: 1,
+			loop: true,
+			spaceBetween: 500,
+		},
+	},
+})
